b3e4c3a9c9827029b0f2d059c0808d80cbfa15c9
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-09-01 18:31:18 -0500

    finish basic app

--- app.js
+++ app.js
@@ -8,15 +8,11 @@ var fetchFotos = function(name) {
         })
 		.then(function(data){
             $.each(data.items, function(i,item){
-                //append image to the grid, wrap with a link tag so it's clickable
                 $("<img/>").attr("src", item.media.m).appendTo("#pics").wrap("<a href=" + item.link + "></a>");
-                //if ( i >= 10 ) return false; //display 9 images
+                if ( i >= 5 ) return false; 
+         
             });
         })
-
-	// return $.get("https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=ad02f4ee57e64cbf39e4457af9f0f188&extras=nigeria&format=json&nojsoncallback=1&auth_token=72157655724395674-1828e8c4e2ab3928&api_sig=ee1a2a089fec28c78642267a0ba552c7", name) {
-	// 	  $( ".result" ).html( data );
-	// 	  alert( "Load was performed." );
 };
 
 
