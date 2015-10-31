98905c6a1924688b6de6596c13d5c7b32d0f238b
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-10-30 22:25:27 -0400

    fix port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 5000;
+
 app.use('/', express.static(__dirname));
-app.listen(5000, function() { console.log('listening on port 5000')});
+app.listen(port, function() { console.log('listening on port '+ port)});
