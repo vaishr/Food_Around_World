77058bcbb1c8139c93e5fb25dc19cec11298609c
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-11-22 14:27:48 -0800

    change pic display layout

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var fetchFotos = function(name) {
         })
 		.then(function(data){
             $.each(data.items, function(i, item){
-                if(i <= 8) {
+                if(i <= 5) {
                 console.log("data.items", data.items);
                 console.log("data", data);
                 $("<img/>").attr("src", item.media.m).appendTo("#pics").wrap("<a href=" + item.link + "></a>");
@@ -19,7 +19,3 @@ function addCommas(intNum) {
   return (intNum + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
 }
 
-
-
-// Key:
-// ada57877f1d9ec39d7da7278e9ff0c95
