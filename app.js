a6d676dfc1ffaed9e9ddcdd63259ab94eee6fdfc
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-12-03 20:25:47 -0800

    add page load spinner

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var fetchFotos = function(name) {
 	return $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-        {
-            tags: name + 'food',
-            format: "json"
-        })
-		.then(function(data){
-            if (!data.items.length) {noPicsMessage(name);};
-            $.each(data.items, function(i, item){
-                if(i <= 5) {
-                console.log("data.items", data.items);
-                console.log("data", data);
-                $("<img/>").attr("src", item.media.m).appendTo("#pics").wrap("<a href=" + item.link + "></a>");
-                }
-            })
-        });
+    {
+        tags: name + 'food',
+        format: "json"
+    })
+  .then(function(data){
+    if (!data.items.length) {noPicsMessage(name);};
+    $.each(data.items, function(i, item){
+        if(i <= 5) {
+            console.log("data.items", data.items);
+            console.log("data", data);
+            $("<img/>").attr("src", item.media.m).appendTo("#pics").wrap("<a href=" + item.link + "></a>");
+        }
+    })
+});
 };
 
 function addCommas(intNum) {
@@ -21,7 +21,10 @@ function addCommas(intNum) {
 }
 
 function noPicsMessage(name) {
-     var message = "<br><p><i>Sorry, could not fetch any pictures of food from " + name + ".  Try another country! :)</i></p>"
-     $('#pics').append(message);
+   var message = "<br><p><i>Sorry, could not fetch any pictures of food from " + name + ".  Try another country! :)</i></p>"
+$('#pics').append(message);
 }
 
+function removeSpinner() {
+    $('.gear_load').hide();
+}
