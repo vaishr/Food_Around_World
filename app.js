78f4f1feb188e39e823b33110f66697b747cab77
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-12-05 15:41:03 -0800

    change population format

--- app.js
+++ app.js
@@ -5,26 +5,23 @@ var fetchFotos = function(name) {
         format: "json"
     })
   .then(function(data){
-    if (!data.items.length) {noPicsMessage(name);};
+    if (!data.items.length) { noPicsMessage(name);};
     $.each(data.items, function(i, item){
         if(i <= 5) {
-            console.log("data.items", data.items);
-            console.log("data", data);
             $("<img/>").attr("src", item.media.m).appendTo("#pics").wrap("<a href=" + item.link + "></a>");
         }
     })
 });
 };
 
-function addCommas(intNum) {
-  return (intNum + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+function formatNum(intNum) {
+  var intNum = (intNum + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,').split(",");
+  return intNum.length > 1 ? (Math.round(parseInt(intNum.join(""), 10) / Math.pow(1000, intNum.length-1)) + " " + ["thousand", "million", "billion"][intNum.length-2]) : intNum[0];
+
 }
 
 function noPicsMessage(name) {
-   var message = "<br><p><i>Sorry, could not fetch any pictures of food from " + name + ".  Try another country! :)</i></p>"
-$('#pics').append(message);
+    var message = "<br><p><i>Sorry, could not fetch any pictures of food from " + name + ".  Try another country :)</i></p>"
+    $('#pics').append(message);
 }
 
-function removeSpinner() {
-    $('.gear_load').hide();
-}
