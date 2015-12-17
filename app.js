c69ba08f9249bd8f461827925333c9a20411b1b3
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-12-16 22:17:58 -0800

    separate search tags

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var fetchFotos = function(name) {
 	return $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
     {
-        tags: name + 'food',
+        tags:  name + ', food',  
+        tagmode: "all",
         format: "json"
     })
   .then(function(data){
-    if (!data.items.length) { noPicsMessage(name);};
+    if (!data.items.length) { noPicsMessage(name); }
     $.each(data.items, function(i, item){
         if(i <= 5) {
             $("<img/>").attr("src", item.media.m).appendTo("#pics").wrap("<a href=" + item.link + "></a>");
         }
     })
-});
+   })
 };
 
 function formatNum(intNum) {
   var intNum = (intNum + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,').split(",");
   return intNum.length > 1 ? (Math.round(parseInt(intNum.join(""), 10) / Math.pow(1000, intNum.length-1)) + " " + ["thousand", "million", "billion"][intNum.length-2]) : intNum[0];
-
 }
 
 function noPicsMessage(name) {
